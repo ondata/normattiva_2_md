c1763defcabf3c3b2f50b6adce0d4bff44625ffa
aborruso <aborruso@gmail.com>
2025-12-27 12:17:48 +0100

    chore: Bump version to 2.0.24 in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read_readme():
 
 setup(
     name="akoma2md",
-    version="2.0.22",
+    version="2.0.24",
     description="⚠️ DEPRECATED - Use 'normattiva2md' instead | Convertitore da XML Akoma Ntoso a Markdown (CLI: normattiva2md)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
